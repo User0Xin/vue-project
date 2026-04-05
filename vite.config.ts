046b1b5094aaa73a48616bed81192ffc1fc7a2e0
User_Xin <786837227@qq.com>
2026-04-05 22:54:04 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,22 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  server: {
+    port: 8080, // 前端开发服务器端口
+    proxy: {
+      // 代理所有以 '/api' 开头的请求
+      '/api': {
+        target: 'http://localhost:8989', // 真实的后端API地址
+        changeOrigin: true, // 改变请求头中的Origin为目标地址，虚拟一个同源身份
+        rewrite: (path) => path.replace(/^\/api/, '') // （可选）重写请求路径，去掉/api前缀
+      }
+    }
+  },
+
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
